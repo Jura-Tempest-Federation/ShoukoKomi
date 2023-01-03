56b0144a8866de0f0ec25defc42567eea1053d36
Void-Great-Emperor <akshaykumarsb177013@gmail.com>
2023-01-03 19:55:58 +0530

    Update is_karma_mongo.py

--- SaitamaRobot/modules/mongo/is_karma_mongo.py
+++ SaitamaRobot/modules/mongo/is_karma_mongo.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from Himawari import db
+from SaitamaRobot import db
 
 karma_statusdb = db.karma_status
 
