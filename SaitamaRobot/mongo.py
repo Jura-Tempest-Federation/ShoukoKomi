d5fe1aa4dbe7f406699913be457eae0cd9dd7779
Void-Great-Emperor <akshaykumarsb177013@gmail.com>
2023-01-03 15:04:37 +0530

    Update mongo.py

--- SaitamaRobot/mongo.py
+++ SaitamaRobot/mongo.py
@@ -3,8 +3,8 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from RUKA import MONGO_DB_URI
-from RUKA.conf import get_int_key, get_str_key
+from SaitamaRobot import MONGO_DB_URI
+from SaitamaRobot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
