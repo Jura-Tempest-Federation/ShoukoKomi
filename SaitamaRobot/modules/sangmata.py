f03e1b44ac47b401f4b06d7d47a643363fe9f07e
Void-Great-Emperor <akshaykumarsb177013@gmail.com>
2023-01-03 06:51:19 +0530

    Update sangmata.py

--- SaitamaRobot/modules/sangmata.py
+++ SaitamaRobot/modules/sangmata.py
@@ -36,7 +36,7 @@ logger = logging.getLogger(__name__)
 
 
 ##Making Updater For TeleCallerBot
-updater=Updater(TOKEN)
+updater = Updater('TOKEN', use_context=True)
 dispatcher= updater.dispatcher
 
 
